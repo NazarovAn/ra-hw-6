09e570faa274ea31f3bee5243831e020701af92d
NazarovAn <naatonchrome@gmail.com>
2020-12-08 00:37:15 +0400

    world clock task done

---
+++ src/components/HW-6.1/Clock.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import './Clock.css'
+
+export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.hoursHand = React.createRef();
+    this.minutesHand = React.createRef();
+    this.secondsHand = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setClock();
+  }
+
+  setClock() {
+    const date = new Date();
+    const hours = date.getHours() + (date.getTimezoneOffset() / 60) + parseInt(this.props.offset, 10);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    
+    this.hoursHand.current.style.rotate = `${ ( hours * 30 ) + ( minutes / 2 ) }deg`;    
+    this.minutesHand.current.style.rotate = `${ minutes * 6 }deg`;
+    this.secondsHand.current.style.rotate = `${ seconds * 6 }deg`;
+
+    this.minutesTimeout = setTimeout(() => {
+      this.minutesHand.current.style.rotate = `${ minutes * 6 + 6 }deg`;
+      this.minutesHand.current.style.animation = 'rotate 3600s infinite steps(60)';
+    }, (60 - seconds) * 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.minutesTimeout);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__name">{ this.props.name }</div>
+        <div className="clock__remove" onClick={ () => this.props.onRemove(this.props.id) }>&#10008;</div>
+        <div className="clock__wrapper">
+          <div className="clock__center"/>
+          <div className="clock__hours_container" ref={ this.hoursHand }>
+            <div className="clock__hours"/>
+          </div>
+          <div className="clock__minutes_container" ref={ this.minutesHand }>
+            <div className="clock__minutes"/>
+          </div>
+          <div className="clock__seconds_container" ref={ this.secondsHand }>
+            <div className="clock__seconds"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/HW-6.1/WorldClock.js
+++ src/components/HW-6.1/WorldClock.js
@@ -1,10 +1,66 @@
+import { nanoid } from 'nanoid';
 import React, { Component } from 'react'
+import Clock from './Clock';
+import './WorldClock.css'
 
 export default class WorldClock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      list: [
+        { name: "Moscow", offset: '+3', id: nanoid() },
+        { name: "New York", offset: '-5', id: nanoid() },
+        { name: "London", offset: '0', id: nanoid() },
+        { name: "Tokio", offset: '+9', id: nanoid() }
+      ],
+      nameInputValue: '',
+      offsetInputValue: '',
+    };
+    this.removeClock = this.removeClock.bind(this);
+    this.handleInputs = this.handleInputs.bind(this);
+    this.addClock = this.addClock.bind(this);
+    this.clearInputs = this.clearInputs.bind(this);
+  }
+
+  addClock(event) {
+    event.preventDefault();
+    if (!this.state.nameInputValue || !this.state.offsetInputValue) {
+      return;
+    }
+
+    this.setState((prev) => ({ ...prev, list: [...prev.list, { name:this.state.nameInputValue, offset: this.state.offsetInputValue, id: nanoid() }] }));
+    this.clearInputs();
+  }
+
+  removeClock(id) {
+    this.setState((prev) => ({ ...prev, list: this.state.list.filter((clock) => clock.id !== id) }));
+  }
+
+  handleInputs(event) {
+    this.setState((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  }
+
+  clearInputs() {
+    this.setState((prev) => ({ ...prev, nameInputValue: '', offsetInputValue: '' }));
+  }
+
   render() {
     return (
-      <div>
-        World Clock
+      <div className="world_clock">
+        <div className="world_clock__inputs_wrapper">
+          <label>
+            <h5 className="world_clock__input_heading">Название</h5>
+            <input className="world_clock__input" value={ this.state.nameInputValue } name="nameInputValue" onChange={ this.handleInputs }/>
+          </label>
+          <label>
+            <h5 className="world_clock__input_heading">Временная зона</h5>
+            <input className="world_clock__input" value={ this.state.offsetInputValue } name="offsetInputValue" onChange={ this.handleInputs }/>
+          </label>
+          <a className="world_clock__add_btn" onClick={ this.addClock } href="/">Добавить</a>
+        </div>
+        <div className="world_clock__list">
+          { this.state.list.map((clock) => <Clock { ...clock } key={ clock.id } onRemove={ this.removeClock } />) }
+        </div>
       </div>
     )
   }
